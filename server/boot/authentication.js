e1d28cd52c38264f1598d5103667dbadba1c33c4
David Wettstein <dwettstein@gmail.com>
2017-07-22 13:59:06 +0200

    Missed one.. dammit

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -8,16 +8,16 @@ module.exports = function enableAuthentication(server) {
 
   var User = server.models.User;
 
-  this.defaultUsername = server.get('defaultUsername');
-  this.defaultPassword = server.get('defaultPassword');
-  if (typeof this.defaultUsername == 'undefined' || this.defaultUsername === null || this.defaultUsername === '' ||
-    typeof this.defaultPassword == 'undefined' || this.defaultPassword === null || this.defaultPassword === '') {
+  var defaultUsername = server.get('defaultUsername');
+  var defaultPassword = server.get('defaultPassword');
+  if (typeof defaultUsername == 'undefined' || defaultUsername === null || defaultUsername === '' ||
+    typeof defaultPassword == 'undefined' || defaultPassword === null || defaultPassword === '') {
     throw 'The default username and password were not found or are empty in config.json file.';
   }
 
   User.upsertWithWhere(
     {where: {email: 'defaultUser@localhost.com'}},
-    {username: this.defaultUsername, password: this.defaultPassword, email: 'defaultUser@localhost.com'},
+    {username: defaultUsername, password: defaultPassword, email: 'defaultUser@localhost.com'},
     function(err, defaultUser) {
       if (err) {
         console.error(err);
@@ -29,7 +29,7 @@ module.exports = function enableAuthentication(server) {
       });
 
       var defaultAccessToken = server.get('defaultAccessToken');
-      if (typeof this.defaultAccessToken == 'undefined' || this.defaultAccessToken === null || this.defaultAccessToken === '') {
+      if (typeof defaultAccessToken == 'undefined' || defaultAccessToken === null || defaultAccessToken === '') {
         console.warn('The default access token was not found or is empty in config.json file. Skip creation of it.');
       } else {
         var ONE_YEAR_IN_SEC = 60 * 60 * 24 * 365;
