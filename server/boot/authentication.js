50992da3b42cea33963daa82708d16a84dc8298c
David Wettstein <dwettstein@gmail.com>
2017-07-22 13:54:05 +0200

    Fix wrong variable assignment

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -8,8 +8,8 @@ module.exports = function enableAuthentication(server) {
 
   var User = server.models.User;
 
-  var defaultUsername = server.get('defaultUsername');
-  var defaultPassword = server.get('defaultPassword');
+  this.defaultUsername = server.get('defaultUsername');
+  this.defaultPassword = server.get('defaultPassword');
   if (typeof this.defaultUsername == 'undefined' || this.defaultUsername === null || this.defaultUsername === '' ||
     typeof this.defaultPassword == 'undefined' || this.defaultPassword === null || this.defaultPassword === '') {
     throw 'The default username and password were not found or are empty in config.json file.';
@@ -17,7 +17,7 @@ module.exports = function enableAuthentication(server) {
 
   User.upsertWithWhere(
     {where: {email: 'defaultUser@localhost.com'}},
-    {username: defaultUsername, password: defaultPassword, email: 'defaultUser@localhost.com'},
+    {username: this.defaultUsername, password: this.defaultPassword, email: 'defaultUser@localhost.com'},
     function(err, defaultUser) {
       if (err) {
         console.error(err);
@@ -29,8 +29,8 @@ module.exports = function enableAuthentication(server) {
       });
 
       var defaultAccessToken = server.get('defaultAccessToken');
-      if (typeof this.defaultUsername == 'undefined' || this.defaultUsername === null || this.defaultUsername === '') {
-        console.warn('The default accesstoken was not found or is empty in config.json file. Skip creation of it.');
+      if (typeof this.defaultAccessToken == 'undefined' || this.defaultAccessToken === null || this.defaultAccessToken === '') {
+        console.warn('The default access token was not found or is empty in config.json file. Skip creation of it.');
       } else {
         var ONE_YEAR_IN_SEC = 60 * 60 * 24 * 365;
         defaultUser.accessTokens.create({
